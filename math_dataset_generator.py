927f505f4bbeb02bdb2ff6ac8cb432fc6997a8a4
Tom Dörr <tomdoerr96@gmail.com>
2024-12-26 14:56:35 +0100

    refactor: Reduce dataset size for testing in math_dataset_generator.py

--- math_dataset_generator.py
+++ math_dataset_generator.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     
     # Generate dataset
     generator = MathDatasetGenerator()
-    dataset = generator.generate_dataset(num_tasks=1000)
+    dataset = generator.generate_dataset(num_tasks=1)
     
     # Save to file
     with open("math_dataset.json", "w") as f:
